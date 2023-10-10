a58973f42bfed988294b7c1dc7b7c7edb4f028a7
dilshadlgf <dilshad.ahmad@logikoof.in>
2023-10-10 15:22:42 +0530

    update-1

---
+++ app/src/main/java/com/example/templatekotlin1/ui/frag/TempFragment.kt
@@ -0,0 +1,17 @@
+package com.example.templatekotlin1.ui.frag
+
+import com.example.templatekotlin1.R
+import com.example.templatekotlin1.base.BaseFragment
+import com.example.templatekotlin1.databinding.FragmentTempBinding
+
+class TempFragment :BaseFragment<FragmentTempBinding>(R.layout.fragment_temp) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+
+}
